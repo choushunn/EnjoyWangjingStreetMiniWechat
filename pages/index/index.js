48410956125d955b1304a716b2046327685a40fd
choushunn <choushunn@stu.cqut.edu.cn>
2023-07-02 12:31:06 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,30 @@
+// index.js
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    PageCur: 'basics'
+  },
+  NavChange(e) {
+    this.setData({
+      PageCur: e.currentTarget.dataset.cur
+    })
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onShareAppMessage() {
+    return {
+      title: 'ColorUI-高颜值的小程序UI组件库',
+      imageUrl: '/images/share.jpg',
+      path: '/pages/index/index'
+    }
+  },
+  onLoad: function () {
+    
+    }
+ 
+})
