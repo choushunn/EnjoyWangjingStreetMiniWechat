394bce0d1b57b62162c40223c76f0cf3b4c20f3b
guo <1249625224@qq.com>
2023-07-09 21:21:15 +0800

    GGGnew

--- app.js
+++ app.js
@@ -52,33 +52,52 @@ App({
     ColorList: [{
         title: '便民电话',
         name: 'red',
-        color: '#e54d42'
+        color: '#e54d42',
+        icon: 'dianhua'
       },
       {
-        title: '特色服务',
+        title: '预约服务',
         name: 'orange',
-        color: '#f37b1d'
+        color: '#f37b1d',
+        icon: 'edit'
       },
       {
-        title: '问题上报',
+        title: '咨询服务',
         name: 'yellow',
-        color: '#fbbd08'
+        color: '#fbbd08',
+        icon: 'comment'
       },
       {
         title: '投诉公示',
         name: 'olive',
-        color: '#8dc63f'
+        color: '#8dc63f',
+        icon: 'newshot'
       },
       {
-        title: '志愿者',
+        title: '反馈意见',
         name: 'green',
-        color: '#39b54a'
+        color: '#39b54a',
+        icon: 'question'
       },
       {
-        title: '工单提交',
+        title: '用户工单',
         name: 'cyan',
-        color: '#1cbbb4'
+        color: '#1cbbb4',
+        icon: 'copy'
+      },
+      {
+        title: '社区通知',
+        name: 'blue',
+        color: '#0081ff',
+        icon: 'notice'
+      },
+      {
+        title: '信息上报',
+        name: 'pink',
+        color: '#e03997',
+        icon: 'upload'
       }
+
     ]
   }
 })
