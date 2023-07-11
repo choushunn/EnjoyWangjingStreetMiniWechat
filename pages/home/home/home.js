f1e1da163f5499bbab0306527421004f1cf1f560
choushunn <choushunn@stu.cqut.edu.cn>
2023-07-11 21:39:13 +0800

    add some ui

--- pages/home/home/home.js
+++ pages/home/home/home.js
@@ -1,31 +1,35 @@
 // pages/basics/home/home.js
+const app = getApp();
 Component({
   options: {
     addGlobalClass: true,
   },
   data: {
+    ColorList:app.globalData.ColorList,
+    TabCur: 0,
+    scrollLeft: 0,
     elements: [{
         title: '办事指南',
-        name: 'instruction',
-        color: 'cyan',
+        name: 'red',
+        color: 'red',
         icon: 'newsfill'
       },
       {
         title: '办事大厅',
-        name: 'office',
-        color: 'blue',
+        name: 'purple',
+        color: 'orange',
         icon: 'servicefill'
       },
       {
         title: '政民互动',
         name: 'interaction',
-        color: 'purple',
+        color: 'green',
         icon: 'group'
       },
       {
         title: '预约服务 ',
         name: 'appointment',
-        color: 'mauve',
+        color: 'pink',
         icon: 'post'
       },
     ],
@@ -35,8 +39,8 @@ Component({
       url: 'https://img1.qunarzz.com/travel/poi/201403/28/daab198c5029a423ddb12cfb.jpg_r_720x400x95_0c4e4c13.jpg'
     }, {
       id: 1,
-        type: 'image',
-        url: 'https://youimg1.c-ctrip.com/target/100i10000000o9w0fE878.jpg',
+      type: 'image',
+      url: 'https://youimg1.c-ctrip.com/target/100i10000000o9w0fE878.jpg',
     }, {
       id: 2,
       type: 'image',
@@ -45,19 +49,17 @@ Component({
       id: 3,
       type: 'image',
       url: 'https://image.scol.com.cn/data/attachment/forum/202105/25/1621945509990.jpg'
-    }, {
-      id: 4,
-      type: 'image',
-      url: 'https://image.scol.com.cn/data/attachment/forum/202105/05/1620216786386.jpg'
-    }, {
-      id: 5,
-      type: 'image',
-      url: 'https://youimg1.c-ctrip.com/target/100v0q000000ghzir4719.jpg'
-    }, {
-      id: 6,
-      type: 'image',
-      url: 'https://image.scol.com.cn/data/attachment/forum/202107/07/1625659035742.jpg'
     }],
-  },
+    newsData:[{
 
+    }]
+  },
+  methods: {
+    tabSelect(e) {
+      this.setData({
+        TabCur: e.currentTarget.dataset.id,
+        scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+      })
+    }
+  }
 })
--- pages/service/home/home.js
+++ pages/service/home/home.js
@@ -1,142 +1,77 @@
 // pages/service/home/home.js
 const app = getApp();
-Page({
-
-  /**
-   * 页面的初始数据
-   */
+Component({
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
-    ColorList:app.globalData.ColorList,
     elements: [{
-
-      title: '办事指南',
-      name: 'layout',
-      color: 'cyan',
-      icon: 'newsfill'
+        title: '办事指南',
+        name: 'layout',
+        color: 'cyan',
+        icon: 'newsfill'
+      },
+      {
+        title: '办事大厅',
+        name: 'background',
+        color: 'blue',
+        icon: 'servicefill'
+      },
+      {
+        title: '政民互动',
+        name: 'text',
+        color: 'purple',
+        icon: 'group'
+      },
+      {
+        title: '预约服务 ',
+        name: 'icon',
+        color: 'mauve',
+        icon: 'post'
+      },
+    ],
+    elements2: [{
+        title: '便民电话',
+        color: 'cyan',
+        icon: 'dianhua'
+      },
+      {
+        title: '社区公示',
+        name: 'background',
+        color: 'blue',
+        icon: 'calendar'
+      },
+      {
+        title: '知识科普',
+        name: 'text',
+        color: 'purple',
+        icon: 'read'
+      },
+    ],
+    elements3: [{
+        title: '电费',
+        color: 'white',
+        icon: 'text'
+      },
+      {
+        title: '水费',
+        color: 'white',
+        icon: 'upload'
+      },
+      {
+        title: '物业费',
+        color: 'white',
+        icon: 'upload'
+      },
+    ],
+  },
+  methods: {
+    isCard(e) {
+      this.setData({
+        isCard: e.detail.value
+      })
     },
-    {
-      title: '办事大厅',
-      name: 'background',
-      color: 'blue',
-      icon: 'servicefill'
+    searchIcon(e) {
+      console.log("搜索。。。。。")
     },
-    {
-      title: '政民互动',
-      name: 'text',
-      color: 'purple',
-      icon: 'group'
-    },
-    {
-      title: '预约服务 ',
-      name: 'icon',
-      color: 'mauve',
-      icon: 'post'
-    },
-  ],
-
-  elements2: [{
-
-    title: '便民电话',
-    color: 'cyan',
-    icon: 'dianhua'
-  },
-  {
-    title: '社区公示',
-    name: 'background',
-    color: 'blue',
-    icon: 'calendar'
-  },
-  {
-    title: '知识科普',
-    name: 'text',
-    color: 'purple',
-    icon: 'read'
-  },
-
-],
-
-elements3: [{
-
-  title: '电费',
-  color: 'white',
-  icon: 'text'
-},
-{
-  title: '水费',
-  color: 'white',
-  icon: 'upload'
-},
-
-{
-  title: '物业费',
-  color: 'white',
-  icon: 'upload'
-},
-],
-  },
-  isCard(e) {
-    this.setData({
-      isCard: e.detail.value
-    })
-  },
-  searchIcon(e) {
-    console.log("搜索。。。。。")
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
   }
 })
