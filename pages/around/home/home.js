1a3aba7fa991ed976a698581c95ed64575449108
choushunn <choushunn@stu.cqut.edu.cn>
2023-07-12 16:42:22 +0800

    修改了用户页

--- pages/around/home/home.js
+++ pages/around/home/home.js
@@ -17,53 +17,93 @@ Component({
     markers: [], // 存储地图上的标记点
     latitude: '', // 当前位置的纬度
     longitude: '', // 当前位置的经度
-    textData: {} // 显示标记点信息的数据
+    textData: {}, // 显示标记点信息的数据
+    dataAddress: [{
+        name: '地点1',
+        desc: '描述1',
+        distance: 1.2
+      },
+      {
+        name: '地点2',
+        desc: '描述2',
+        distance: 2.5
+      },
+      {
+        name: '地点3',
+        desc: '描述3',
+        distance: 3.8
+      },
+      {
+        name: '地点4',
+        desc: '描述4',
+        distance: 4.1
+      },
+      {
+        name: '地点5',
+        desc: '描述5',
+        distance: 5.3
+      }
+    ]
   },
   created: function () {
-    var that = this;
-    // 创建高德地图 API 实例
-    var myAmapFun = new AMapWX({
-      key: Config.key // 设置应用密钥
-    });
-
-    // 开始监听位置变化
-    wx.startLocationUpdate({
-      success: function () {
-        wx.onLocationChange(function (res) {
-          // 获取最新位置信息
-          var latitude = res.latitude;
-          var longitude = res.longitude;
-
-          // 调用高德地图 API 获取周边 POI 数据
-          myAmapFun.getPoiAround({
-            location: longitude + ',' + latitude, // 设置查询位置
-            iconPathSelected: '../../../images/amap/marker_checked.png', //如：..­/..­/img/marker_checked.png
-            iconPath: '../../../images/amap/marker.png', //如：..­/..­/img/marker.png
-            success: function (data) {
-              that.setData({
-                markersData: data.markers, // 存储获取到的周边POI数据
-                markers: data.markers, // 存储地图上的标记点
-                latitude: latitude, // 存储当前位置的纬度
-                longitude: longitude // 存储当前位置的经度
-              });
-              that.showMarkerInfo(data.markers, 0); // 显示第一个标记点的信息
-            },
-            fail: function (info) {
-              wx.showModal({
-                title: info.errMsg // 显示错误信息
-              })
-            }
-          })
-        })
-      },
-      fail: function (info) {
-        wx.showModal({
-          title: info.errMsg // 显示错误信息
-        })
-      }
-    })
+    this.startLocation();
   },
   methods: {
+    startLocation: function () {
+      var that = this;
+      // 创建高德地图 API 实例
+      var myAmapFun = new AMapWX({
+        key: Config.key // 设置应用密钥
+      });
+      // 开始监听位置变化
+      wx.startLocationUpdate({
+        success: function () {
+          wx.onLocationChange(function (res) {
+            // 获取最新位置信息
+            var latitude = res.latitude;
+            var longitude = res.longitude;
+            // 调用高德地图 API 获取周边 POI 数据
+            myAmapFun.getPoiAround({
+              location: longitude + ',' + latitude, // 设置查询位置
+              iconPathSelected: '../../../images/amap/marker_checked.png', //如：..­/..­/img/marker_checked.png
+              iconPath: '../../../images/amap/marker.png', //如：..­/..­/img/marker.png
+              success: function (data) {
+                that.setData({
+                  markersData: data.markers, // 存储获取到的周边POI数据
+                  markers: data.markers, // 存储地图上的标记点
+                  latitude: latitude, // 存储当前位置的纬度
+                  longitude: longitude // 存储当前位置的经度
+                });
+                that.showMarkerInfo(data.markers, 0); // 显示第一个标记点的信息
+              },
+              fail: function (info) {
+                wx.showModal({
+                  title: info.errMsg // 显示错误信息
+                })
+              }
+            })
+          })
+        },
+        fail: function (info) {
+          wx.showModal({
+            title: info.errMsg // 显示错误信息
+          })
+        }
+      })
+    },
+    backToLocation: function() {
+      console.log("返回当前地址")// 将地图中心移动到当前定位点的位置
+    },
+    showModal(e) {
+      this.setData({
+        modalName: e.currentTarget.dataset.target
+      })
+    },
+    hideModal(e) {
+      this.setData({
+        modalName: null
+      })
+    },
     // 点击标记点时触发的事件处理函数
     makertap: function (e) {
       var index = e.markerId;
--- pages/community/home/home.js
+++ pages/community/home/home.js
@@ -6,49 +6,57 @@ Component({
       title: '便民电话',
       name: 'red',
       color: '#e54d42',
-      icon: 'dianhua'
+      icon: 'dianhua',
+      url: 'dianhua'
     },
     {
       title: '预约服务',
       name: 'orange',
       color: '#f37b1d',
-      icon: 'edit'
+      icon: 'edit',
+      url: 'dianhua'
     },
     {
       title: '咨询服务',
       name: 'yellow',
       color: '#fbbd08',
-      icon: 'comment'
+      icon: 'comment',
+      url: 'dianhua'
     },
     {
       title: '投诉公示',
       name: 'olive',
       color: '#8dc63f',
-      icon: 'newshot'
+      icon: 'newshot',
+      url: 'dianhua'
     },
     {
       title: '反馈意见',
       name: 'green',
       color: '#39b54a',
-      icon: 'question'
+      icon: 'question',
+      url: 'dianhua'
     },
     {
       title: '用户工单',
       name: 'cyan',
       color: '#1cbbb4',
-      icon: 'copy'
+      icon: 'copy',
+      url: 'dianhua'
     },
     {
       title: '社区通知',
       name: 'blue',
       color: '#0081ff',
-      icon: 'notice'
+      icon: 'notice',
+      url: 'dianhua'
     },
     {
       title: '信息上报',
       name: 'pink',
       color: '#e03997',
-      icon: 'upload'
+      icon: 'upload',
+      url: 'dianhua'
     }
   ]
   },
