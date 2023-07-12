8eaf9163d20eb0ffa47a056bd3e7ccc176ec43d7
choushunn <choushunn@stu.cqut.edu.cn>
2023-07-12 22:14:41 +0800

    改变了社区页面的布局

--- pages/community/home/home.js
+++ pages/community/home/home.js
@@ -2,61 +2,66 @@
 const app = getApp();
 Component({
   data:{
-    elements: [{
-      title: '便民电话',
-      name: 'red',
-      color: '#e54d42',
-      icon: 'dianhua',
-      url: 'dianhua'
+    elements: [
+      {
+        title: '社区通知',
+        name: 'notice',
+        color: 'olive',
+        icon: 'notice',
+        url: 'notice'
+      },{
+        title: '社区活动',
+        name: 'activity',
+        color: 'green',
+        icon: 'group',
+        url: 'activity'
+      },{
+      title: '居民报修',
+      name: 'repair',
+      color: 'cyan',
+      icon: 'paint',
+      url: 'repair'
     },
     {
       title: '预约服务',
-      name: 'orange',
-      color: '#f37b1d',
+      name: 'appointment',
+      color: 'red',
       icon: 'edit',
-      url: 'edit'
+      url: 'appointment'
     },
     {
       title: '咨询服务',
-      name: 'yellow',
-      color: '#fbbd08',
+      name: 'consult',
+      color: 'orange',
       icon: 'comment',
-      url: 'comment'
-    },
-    {
-      title: '投诉公示',
-      name: 'olive',
-      color: '#8dc63f',
-      icon: 'newshot',
-      url: 'newshot'
-    },
-    {
-      title: '反馈意见',
-      name: 'green',
-      color: '#39b54a',
-      icon: 'question',
-      url: 'question'
-    },
-    {
-      title: '用户工单',
-      name: 'cyan',
-      color: '#1cbbb4',
-      icon: 'copy',
-      url: 'copy'
-    },
-    {
-      title: '社区通知',
-      name: 'blue',
-      color: '#0081ff',
-      icon: 'notice',
-      url: 'notice'
+      url: 'consult'
     },
     {
       title: '信息上报',
-      name: 'pink',
-      color: '#e03997',
+      name: 'report',
+      color: 'yellow',
       icon: 'upload',
-      url: 'upload'
+      url: 'report'
+    },{
+      title: '便民电话',
+      name: 'phone',
+      color: 'blue',
+      icon: 'dianhua',
+      url: 'phone'
+    },
+    {
+      title: '投诉公示',
+      name: 'complaint',
+      color: 'purple',
+      icon: 'newshot',
+      url: 'complaint'
+    },
+    {
+      title: '反馈意见',
+      name: 'feedback',
+      color: 'mauve',
+      icon: 'question',
+      url: 'feedback'
     }
   ]
   },
