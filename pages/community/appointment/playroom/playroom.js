eea37da36b35ab80edbd2c7533361cc34ee8daea
guo <1249625224@qq.com>
2023-07-13 15:04:35 +0800

    预约服务

---
+++ pages/community/appointment/playroom/playroom.js
@@ -0,0 +1,91 @@
+// pages/community/appointment/playroom/playroom.js
+const app = getApp();
+var now = new Date();
+// 格式化时间
+var year = now.getFullYear();
+var month = now.getMonth() + 1;
+var day = now.getDate();
+var hour = now.getHours();
+var minute = now.getMinutes();
+var second = now.getSeconds();
+// 将时间格式化为字符串
+var timeStr =  hour + ':' + minute ;
+var dateStr =  year + '-' + month + '-' + day ;
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    time: timeStr,
+    date: dateStr,
+  },
+  TimeChange(e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  DateChange(e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
