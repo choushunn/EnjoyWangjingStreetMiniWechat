32916f6d342cd720598feea9e295ca6c84cbd0fc
choushunn <choushunn@stu.cqut.edu.cn>
2023-07-23 16:58:46 +0800

    Color GA

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+    // 检查本地存储中是否有用户信息
+    var userInfo = wx.getStorageSync('userInfo');
+    if (userInfo) {
+      // 如果有用户信息，则设置登录状态和用户信息
+      this.globalData.isLoggedIn = true;
+      this.globalData.userInfo = userInfo;
+    }
     // 登录
     wx.login({
       success: res => {
@@ -22,7 +24,6 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -49,55 +50,6 @@ App({
   },
   globalData: {
     userInfo: null,
-    ColorList: [{
-        title: '便民电话',
-        name: 'red',
-        color: '#e54d42',
-        icon: 'dianhua'
-      },
-      {
-        title: '预约服务',
-        name: 'orange',
-        color: '#f37b1d',
-        icon: 'edit'
-      },
-      {
-        title: '咨询服务',
-        name: 'yellow',
-        color: '#fbbd08',
-        icon: 'comment'
-      },
-      {
-        title: '投诉公示',
-        name: 'olive',
-        color: '#8dc63f',
-        icon: 'newshot'
-      },
-      {
-        title: '反馈意见',
-        name: 'green',
-        color: '#39b54a',
-        icon: 'question'
-      },
-      {
-        title: '用户工单',
-        name: 'cyan',
-        color: '#1cbbb4',
-        icon: 'copy'
-      },
-      {
-        title: '社区通知',
-        name: 'blue',
-        color: '#0081ff',
-        icon: 'notice'
-      },
-      {
-        title: '信息上报',
-        name: 'pink',
-        color: '#e03997',
-        icon: 'upload'
-      }
-
-    ]
+    isLoggedIn: false,
   }
 })
