16e917f32c026e2a5798888f9210bbd84a439ccf
guo <1249625224@qq.com>
2023-07-23 21:38:19 +0800

    map

--- pages/around/home/home.js
+++ pages/around/home/home.js
@@ -23,6 +23,7 @@ Component({
     hot_list: ['服务', '出行', '设施'],
     area_list: ['所有','100m','200m'],
     list: [],
+    tips:[],
     dataAddress: [{
       name: '维也纳国际酒店',
       address: '重庆市两江新区龙兴镇普福大道481号',
@@ -125,6 +126,8 @@ Component({
     },
     backToLocation: function() {
       console.log("返回当前地址")// 将地图中心移动到当前定位点的位置
+      const myMap = wx.createMapContext('myMap');
+      myMap.moveToLocation();
     },
     showModal(e) {
       this.setData({
@@ -170,11 +173,40 @@ Component({
       });
     }
   },
+  bindInput: function(e){
+    var that = this;
+    var keywords = e.detail.value; 
+    var key = config.Config.key;
+    var myAmapFun = new AMapWX({
+      key: Config.key // 设置应用密钥
+    });
+    myAmapFun.getInputtips({
+      keywords: keywords,
+      location: '',
+      success: function(data){
+        if(data && data.tips){
+          that.setData({
+            tips: data.tips
+          });
+        }
+      }
+    })
+  },
+  bindSearch: function(e){
+    var keywords = e.target.dataset.keywords;
+    var url = '../poi/poi?keywords=' + keywords;
+    console.log(url)
+    wx.redirectTo({
+      url: url
+    })
+  },
   getSelecte: function(event) {
     const content = event.currentTarget.dataset.content
     this.setData({
       selected: content
     })
-  }
+  },
+  
+  
   
 })
