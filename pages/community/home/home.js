ce5b9771cb14a27450ac9fbb2a24af50c0f866eb
choushunn <choushunn@stu.cqut.edu.cn>
2023-07-31 16:44:26 +0800

    Spring 提交了代码

--- pages/community/home/home.js
+++ pages/community/home/home.js
@@ -7,59 +7,59 @@ Component({
         title: '通知公告',
         color: 'olive',
         icon: 'notice',
-        url: 'notice'
+        url: '/pages/community/notice/notice'
       },{
         title: '社区活动',
         color: 'green',
         icon: 'group',
-        url: 'activity'
+        url: '/pages/community/activity/activity'
       },{
       title: '居民服务',
       name: 'work',
       color: 'cyan',
       icon: 'paint',
-      url: 'work'
+      url: '/pages/community/work/work'
     },
     {
       title: '预约服务',
       name: 'appointment',
       color: 'red',
       icon: 'edit',
-      url: 'appointment'
+      url: '/pages/community/appointment/appointment'
     },
     {
       title: '咨询服务',
       name: 'consult',
       color: 'orange',
       icon: 'comment',
-      url: 'consult'
+      url: '/pages/community/consult/consult'
     },
     {
       title: '问题上报',
       name: 'report',
       color: 'yellow',
       icon: 'upload',
-      url: 'report'
+      url: '/pages/community/report/report'
     },{
       title: '便民电话',
       name: 'phone',
       color: 'blue',
       icon: 'dianhua',
-      url: 'phone'
+      url: '/pages/community/phone/phone'
     },
     {
       title: '意见反馈',
       name: 'feedback',
       color: 'mauve',
       icon: 'question',
-      url: 'feedback'
+      url: '/pages/community/feedback/feedback'
     },
     {
       title: '近期新闻',
       name: 'feedback',
       color: 'mauve',
       icon: 'question',
-      url: 'feedback'
+      url: '/pages/home/news/news'
     }
   ]
   },
