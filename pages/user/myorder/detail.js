c02873b4b82f070a5d30d269bab7c471abe2f018
unknown <Spring>
2024-01-15 00:55:59 +0800

    fix:detail

--- pages/user/myorder/detail.js
+++ pages/user/myorder/detail.js
@@ -6,11 +6,12 @@ Page({
    */
   data: {
     count: 5,
-    active: 0,
-    score: 1,
+    active: 1,
+    score: 5,
     item:'',
     apiUri:app.globalData.apiUri
   }, // 预览图片
+
   previewImage(e) {
     const current = e.currentTarget.dataset.src;
     const urls = this.data.imgList.map((item) => {
@@ -36,18 +37,78 @@ Page({
     this.triggerEvent("click", {
       result: score / count * active
     }, {})
+    console.log(this.data);
+    // 点击以后更新这个分数到后台
+    const data = {
+      ticket: this.data.item.id,
+      rating: active
+    };    
+    console.log(data)
+    wx.request({      
+      url: app.globalData.apiUri + 'work_review/',
+      data: data,
+      method: 'POST',
+      header: {
+        "authorization": "Bearer " + wx.getStorageSync('token')
+      },
+      success: function (res) {
+        console.log("服务评价数据：", res); // 打印后端API返回的数据
+        // 处理成功提示信息
+        if (res.statusCode == 201) {
+          wx.showToast({
+            title: '评价成功',
+            success(res) {
+              console.log(res)          
+            },
+            fail(res) {
+              wx.showToast({
+                title: '评价失败',
+              })
+            }
+          })
+        } else {
+          wx.showToast({
+            title: '提交失败',
+          })
+        }
+      },
+      fail: function (res) {
+        console.log(res.errMsg); // 打印错误信息
+        
+        // 处理失败提示信息
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    console.log(options)
+    console.log("传递数据", options)
+    const work_id = options.id
+    var rating = 5
+    var that = this
+    // 尝试从本地存储中获取 count 值
+    wx.request({
+      url: app.globalData.apiUri + 'work_review/?ticket='+work_id,
+      method: 'GET',
+      header:{
+        "authorization":"Bearer "+ wx.getStorageSync('token')
+      },
+      success(res) {
+        console.log("我的评价数据获取成功", res.data)
+        rating = res.data[0] && res.data[0].rating || 5;
+        that.setData({
+          active: rating
+        })
+      }
+    })
     const itemObject = JSON.parse(options.item);
     console.log(itemObject);
     this.setData({
       item:itemObject
     })
+
   },
 
   /**
